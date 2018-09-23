2b18b6ff901d1148b3378583812321db6c617afd
anders <andersge92@gmail.com>
2018-09-23 16:28:02 +0200

    Added a few comments, ensure up to date online

--- Project2/tridiagMatrix.cpp
+++ Project2/tridiagMatrix.cpp
@@ -2,7 +2,7 @@
 #include <armadillo>
 
 using namespace arma;
-//using namespace std;
+using namespace std;
 
 int main()
 {
@@ -11,12 +11,15 @@ int main()
     double h = (double)(1.0/n);
     double d = 2.0/h;
     double a = -1.0/h;
+    double eps_avg;
    
     //creating and filling matrices
-    mat A = zeros(n, n);
-    vec lmbd = zeros(n);
-    vec eps = zeros(n);
-    for( int i = 1; i < (n-1); i++){
+    mat A = zeros(n, n); //what will be our tridiagonal Toeplitz matrix
+    vec lmbd = zeros(n); //our analytical eigenvalues
+    vec eps = zeros(n); //relative error between numerical and analytical eigenvalues. 
+    for( int i = 1; i < (n-1); i++)
+    {
+        // filling Toeplitz matrix
         A(i,i) = d;
         A(i, i+1) = a;
         A(i, i-1) = a;
@@ -24,17 +27,30 @@ int main()
         lmbd(i) = d + 2*a*cos(i*M_PI/(n+1));
     }
 
-    // supplying remaining tridiagonal elements
+    // supplying the ends with appropriate appropriate  elements
     A(0,0) = d;
     A(0,1) = a;
     A(n-1,n-1) = d;
     A(n-1,n-2) = a;
     
+
+
+
     // extracting numerical eigenvalues with armadillo
     vec eigval = eig_sym(A);
-    eigval.print();
 
-    for( int i = 1; i < n; i++){
+    for( int i = 1; i < n-1; i++)
+    {
         eps(i) = fabs(eigval(i) - lmbd(i)) / lmbd(i);
     }
+    // averaging difference
+    for( int i=0; i<n-1; i++)
+    {
+        eps_avg += eps(i); 
+    }
+    eps_avg /= n; 
+    cout << "average error: " << eps_avg << endl;
+
+
+
 }
