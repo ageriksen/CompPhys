1d1c3256d1e7f81f979bd8fb4ad5e481bd77ce60
anders <andersge92@gmail.com>
2018-09-27 16:42:03 +0200

    added function for jacobi rotation and maximum offdiagonal element from http://compphysics.github.io/ComputationalPhysics/doc/pub/eigvalues/html/eigvalues.html

--- Project2/tridiagMatrix.cpp
+++ Project2/tridiagMatrix.cpp
@@ -31,3 +31,76 @@ int main()
     // supplying the ends with appropriate appropriate  elements
     A(0,0) = d; A(0,1) = a; A(n-1,n-1) = d; A(n-1,n-2) = a;
 }
+
+void jacobi_rotate( mat & A, mat & R, int & k, int & l, int n )
+{
+    //declarations of sin and cos
+    double s, c;
+    // general jacobi_rotate algo
+    if ( A(k,l) != 0.0 )
+    {
+        double t, tau; 
+        tau = ( A(l,l) - A(k,k) )/( 2*A(k,l) );
+        // avoiding loss of precision from difference between similar,
+        // large numbers. 
+        if ( tau >= 0 )
+        {
+            t = 1.0/( tau + sqrt( 1.0 + (tau*tau) ) );
+        } 
+        else
+        {
+            t = -1.0/( -tau + sqrt( 1.0 + (tau*tau) ) );
+        }
+        c = 1/sqrt( 1 + (t*t) );
+        s = c*t;
+    }
+    else 
+    {
+        c = 1.0;
+        s = 0.0;
+    }
+    double a_kk, a_ll, a_ik, a_il, r_ik, r_il;
+    a_kk = A(k,k);
+    a_ll = A(l,l);
+    A(k,k) = (c*c)*a_kk - 2.0*(c*s)*A(k,l) + (s*s)*a_ll; 
+    A(l,l) = (s*s)*a_kk - 2.0*(c*s)*A(k,l) + (c*c)*a_ll; 
+    // setting all nondiagonal elements manually
+    A(k,l) = 0.0;
+    A(l,k) = 0.0;
+    for ( int i = 0; i < n; i++)
+    {
+        if ( i != k && i != l)
+        {
+            a_ik = A(i,k);
+            a_il = A(i,l);
+            A(i,k) = c*a_ik - s*a_il;
+            A(i,l) = c*a_il - s*a_ik;
+            A(k,i) = A(i,k);
+            A(l,i) = A(i,l);
+        }
+        // adjusting the eigenvectors
+        r_ik = R(i,k);
+        r_il = R(i,l);
+
+        R(i,k) = c*r_ik - s*r_il;
+        R(i,l) = c*r_il + s*r_ik;
+    }
+} //end of jacobi_rotate
+
+// off diagonal function using armadillo to find largest 
+// offdiagonal element
+void offdiag( mat & A, int & p, int & q, int n )
+{
+    double max; 
+    for ( int i = 0; i < n; i++)
+    {
+        for ( int j = i+1; j < n; j++)
+        {
+            double aij = fabs( A(i,j) );
+            if ( aij > max )
+            {
+                max = aij; p = i; q = j;
+            }
+        }
+    }
+}
