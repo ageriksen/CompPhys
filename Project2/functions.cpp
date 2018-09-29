7de8bc5331c7642445a97ceb7506272c7aded2d0
anders <andersge92@gmail.com>
2018-09-29 15:34:41 +0200

    implemented test of orthogonality of eigenvectors under rotation. began preparation for expansion into QM syst.

--- Project2/functions.cpp
+++ Project2/functions.cpp
@@ -2,16 +2,20 @@
 
 int main(int argc, char *argv[])
 {
-    
+//    if (argc < 2)
+//    {
+//        cout << "you need to provide arguments. "
+//    }
     int dim = 5;
-    double diagonal, semidiagonal, tolerance;
+    double diagonal, semidiagonal, tolerance, step;
     mat eigvalmatrix, eigvecmatrix;
     diagonal = 2.0; semidiagonal = -1.0;
-    setup(dim, diagonal, semidiagonal, eigvalmatrix, eigvecmatrix);
     tolerance = 1.0e-10;
+
+    setup(dim, step, diagonal, semidiagonal, eigvalmatrix, eigvecmatrix);
     wrapper( tolerance, eigvalmatrix, eigvecmatrix, dim);
 
-    if (argc > 1)
+    if (argc > 2)
     {
         int argument = atoi(argv[1]);
         cout << "additional raguments mean initiating tests. " << endl;
@@ -26,16 +30,21 @@ int main(int argc, char *argv[])
                 "of Toeplitz matrix." << endl;
             test_eigvalues();
         }
+        else if (argument == 2)
+        {
+            cout << "2 tests orthogonality of eigenvectors under rotation." << endl;
+            test_orthogonality();
+        }
     }
     return 0;
 } // end of main function
 
 //############################################################
 //setup function to set up necessary variables.
-void setup(int dim, double & diagonal, double & semidiagonal, mat & eigvalmatrix, mat & eigvecmatrix )
+void setup(int dim, double & step, double & diagonal, double & semidiagonal, mat & eigvalmatrix, mat & eigvecmatrix )
 {
     //setting diagonal and semidiagonal element values
-    double diag, semidiag, step; 
+    double diag, semidiag; 
     diag = diagonal; semidiag = semidiagonal;
     step = (double)(1.0/dim); // step size
     diagonal = diag/(step*step);  // diagonal elements
@@ -43,8 +52,9 @@ void setup(int dim, double & diagonal, double & semidiagonal, mat & eigvalmatrix
     //filling matrices
     eigvalmatrix = zeros<mat>(dim, dim); //what will be our tridiagonal Toeplitz matrix
     eigvecmatrix = eye<mat>(dim,dim); // identity matrix to contain our eigenvectors
-    Toeplitztridiag( eigvalmatrix, dim, diag, semidiag ); // tridiagonalizing eigenvalues
+    Toeplitztridiag( eigvalmatrix, dim, diagonal, semidiagonal ); // tridiagonalizing eigenvalues
 } // end of setup
+
 //#############################################################
 //Wrapper function to loop over iterations of rotations
 void wrapper(double tolerance, mat & eigvalmatrix, mat & eigvecmatrix, int dim )
@@ -58,6 +68,7 @@ void wrapper(double tolerance, mat & eigvalmatrix, mat & eigvecmatrix, int dim )
         jacobi_rotate( eigvalmatrix , eigvecmatrix , p, q, dim);
     }
 }
+
 //#############################################################
 //function to set a tridiagonal Toeplitx matrix given diagonal 
 //and "neighbouring" elements values d and a
@@ -73,6 +84,7 @@ void Toeplitztridiag(mat & Matrix, int n, double d, double a)
     Matrix(0,0) = d; Matrix(0,1) = a; 
     Matrix(n-1,n-1) = d; Matrix(n-1,n-2) = a;
 } // end of Toeplitztridiag
+
 //#############################################################
 //function using orthogonal rotations of collumns to produce a
 //tridiagonal matrix within a certain tolerance.
@@ -80,7 +92,7 @@ void jacobi_rotate( mat & A, mat & R, int & k, int & l, int n )
 {
     //declarations of sin and cos
     double s, c;
-    // general jacobi_rotate algo
+    
     if ( A(k,l) != 0.0 )
     {
         double t, tau; 
@@ -103,11 +115,12 @@ void jacobi_rotate( mat & A, mat & R, int & k, int & l, int n )
         c = 1.0;
         s = 0.0;
     }
+
     double a_kk, a_ll, a_ik, a_il, r_ik, r_il;
     a_kk = A(k,k);
     a_ll = A(l,l);
     A(k,k) = (c*c)*a_kk - 2.0*(c*s)*A(k,l) + (s*s)*a_ll; 
-    A(l,l) = (s*s)*a_kk - 2.0*(c*s)*A(k,l) + (c*c)*a_ll; 
+    A(l,l) = (s*s)*a_kk + 2.0*(c*s)*A(k,l) + (c*c)*a_ll; 
     // setting all nondiagonal elements manually
     A(k,l) = 0.0;
     A(l,k) = 0.0;
@@ -118,7 +131,7 @@ void jacobi_rotate( mat & A, mat & R, int & k, int & l, int n )
             a_ik = A(i,k);
             a_il = A(i,l);
             A(i,k) = c*a_ik - s*a_il;
-            A(i,l) = c*a_il - s*a_ik;
+            A(i,l) = c*a_il + s*a_ik;
             A(k,i) = A(i,k);
             A(l,i) = A(i,l);
         }
@@ -130,6 +143,7 @@ void jacobi_rotate( mat & A, mat & R, int & k, int & l, int n )
         R(i,l) = c*r_il + s*r_ik;
     }
 } //end of jacobi_rotate
+
 //####################################################
 // function using armadillo to find largest 
 // offdiagonal element
@@ -152,6 +166,7 @@ void offdiag( mat A, int & p, int & q, int n )
 // ########################################################
 // ########################################################
 // TESTS
+
 // ################################
 // test of offdiagonal max value function
 void test_maxoffdiag()
@@ -176,6 +191,7 @@ void test_maxoffdiag()
     cout << "offdiag returns values of p, q as " << test_p << ", " << test_q << endl;
     cout << "which corresponds to a maxvalue of " << testmatrix(test_p, test_q) << endl;
 } // end of test_maxoffdiag
+
 // ##################################
 // test that correct eigenvalues extracted. 
 void test_eigvalues()
@@ -187,31 +203,65 @@ void test_eigvalues()
      * the project 2 pdf. 
      */
     int dim;
-    double stepsize, diagonal, offdiagonal, tolerance, maxnondiagonal, epsilon;
-    mat testmatrix, testeigmatrix;
-    vec lambda;
-    dim = 5;
-    stepsize = (double) 1.0/dim; diagonal = 2.0/(stepsize*stepsize); offdiagonal = -1.0/(stepsize*stepsize);
-    testmatrix = zeros<mat>(dim, dim); testeigmatrix = eye<mat>(dim, dim);
-    Toeplitztridiag(testmatrix, dim, diagonal, offdiagonal);
-    tolerance = 1.0e-15; maxnondiagonal = 1;
+    double diagonal, semidiagonal, tolerance, epsilon, step;
+    mat testeigvalmatrix, testeigvecmatrix;
+    vec lambda, testeigvalvector;
+    dim = 5; diagonal = 2.0; semidiagonal = -1.0; 
+    tolerance = 1.0e-15; 
+    
+    setup(dim, step, diagonal, semidiagonal, testeigvalmatrix, testeigvecmatrix);
+
+    wrapper( tolerance, testeigvalmatrix, testeigvecmatrix, dim);
+    
     lambda = zeros<vec>(dim);
+    testeigvalvector = zeros<vec>(dim);
     for ( int i = 0; i < dim; i++)
     {
-        lambda(i) = diagonal + 2*offdiagonal*cos(i*M_PI)/(dim+1);
+        lambda(i) = diagonal + 2*semidiagonal*cos( (double(i)+1)*M_PI /( double(dim)+1 ));
+        testeigvalvector(i) = testeigvalmatrix(i,i);
     }
-    while (maxnondiagonal > tolerance )
-    {
-        int test_q, test_p;
-        offdiag(testmatrix, test_p, test_q, dim);
-        maxnondiagonal = fabs(testmatrix(test_q, test_p));
-        jacobi_rotate(testmatrix, testeigmatrix, test_p, test_q, dim);
-    }
-    for (int i = 0; i < dim; i++)
-    {
-        epsilon += (testmatrix(i,i) - lambda(i))/lambda(i);
-    }
-    epsilon = epsilon/dim;
+    epsilon = (mean(testeigvalvector)-mean(lambda))/mean(lambda); // relative difference between means
+                                                                  // of analyrical and numerical eigvals.
+    
+    cout << "test eigenvalue matrix" << endl;
+    cout << testeigvalmatrix << endl;
+    cout << "analytical eigenvalue vector lambda: " << endl;
+    cout << lambda << endl;
     cout << "the average error between analytical and numerical eigenvalues are:" <<
         epsilon << endl;
 } // end of test_eigvalues
+
+// ###########################################
+// test that jacobi rotations maintain orthogonality.
+void test_orthogonality()
+{
+    /*
+     * The test aims to ensure that each column in the transformed matrix maintains
+     * orthogonality under rotations. The test takes a small matrix and ensures that 
+     * columns are orthogonal before and after rotation.
+     */
+    int dim;
+    double diagonal, semidiagonal, tolerance, testinnerproduct, step;
+    mat testeigvalmatrix, testeigvecmatrix;
+    vec testeigveccollumn_i, testeigveccollumn_j;
+    dim = 5; diagonal = 2.0; semidiagonal = -1.0; 
+    tolerance = 1.0e-15; 
+
+    setup( dim, step, diagonal, semidiagonal, testeigvalmatrix, testeigvecmatrix);
+
+    testeigveccollumn_i = testeigvecmatrix.col(1);
+    testeigveccollumn_j = testeigvecmatrix.col(3);
+    testinnerproduct = dot(testeigveccollumn_i, testeigveccollumn_j);
+
+    cout << "our initial Toeplitz matrix has an innerproduct between two collumns of" << endl;
+    cout << testinnerproduct << endl;
+
+    wrapper( tolerance, testeigvalmatrix, testeigvecmatrix, dim );
+
+    testeigveccollumn_i = testeigvecmatrix.col(1);
+    testeigveccollumn_j = testeigvecmatrix.col(3);
+    testinnerproduct = dot(testeigveccollumn_i, testeigveccollumn_j);
+
+    cout << "our rotated matrix has an innerproduct between two collumns of" << endl;
+    cout << testinnerproduct << endl;
+} // end of test_orthogonality
--- Project2/functions.h
+++ Project2/functions.h
@@ -7,7 +7,7 @@
 using namespace std;
 using namespace arma;
 
-void setup(int , double & , double & , mat & , mat & );
+void setup(int , double &, double & , double & , mat & , mat & );
 void wrapper(double , mat & , mat & , int );
 void Toeplitztridiag(mat &, int, double, double);
 void jacobi_rotate( mat &, mat & , int & , int & , int  );
@@ -15,4 +15,5 @@ void offdiag( mat  , int & , int & , int );
 
 void test_maxoffdiag();
 void test_eigvalues();
+void test_orthogonality();
 #endif
