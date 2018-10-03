2d1fdeb47f78a19f36bfb383f68b3c29c6276a71
anders <andersge92@gmail.com>
2018-10-03 22:16:52 +0200

    mostly gathered what I need. haven't implemented variations in rhomax, though. Probably going to show that the functions are close to 0 at this extreme and leave it at that. Properly reads info about 2 particle HO system.

--- Project2/plot.py
+++ Project2/plot.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read(path):
-    collumn1 = []; collumn2 = []; collumn3 = []
+def read2particle(path):
+    collumn1 = []
     filename = open(path, 'r')
     filename.readline()
     filelines = filename.readlines()
     for line in filelines:
         words = line.split()
         collumn1.append(words[0])
-        collumn2.append(words[1])
-        collumn3.append(words[2])
         if (len(words) == 7):
             stepnumber = int(words[-1])
     print("number of steps: ", stepnumber)
-    return stepnumber, np.array(collumn1), np.array(collumn2), np.array(collumn3)
+    return stepnumber, np.array(collumn1)
 
     
 def plot_bucklingbeam():
@@ -25,9 +23,21 @@ def plot_1particleHO():
 
 
 def plot_2particleHO():
+    number = np.zeros(4)
+    path = []
+    for i in range(4):
+        print( i )
+        path.append( "omega"+str(i)+"dim200Eigval.txt")
+    number[0], stateomega0 = read2particle(path[0])
+    number[1], stateomega0 = read2particle(path[1])
+    number[2], stateomega0 = read2particle(path[2])
+    number[3], stateomega0 = read2particle(path[3])
+    step = 10./number
+    
+
     return 0
 
 
 if __name__ == '__main__':
     import sys
-    step, state1, state2, state3 = read(sys.argv[1])
+    plot_2particleHO()
