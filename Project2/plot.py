0344dbfb781b0ba78f9829b62e956d3b1e987573
anders <andersge92@gmail.com>
2018-10-04 11:01:20 +0200

    finalized plot function for 2 particle HO

--- Project2/plot.py
+++ Project2/plot.py
@@ -8,7 +8,7 @@ def read2particle(path):
     filelines = filename.readlines()
     for line in filelines:
         words = line.split()
-        collumn1.append(words[0])
+        collumn1.append(float(words[0]))
         if (len(words) == 7):
             stepnumber = int(words[-1])
     print("number of steps: ", stepnumber)
@@ -29,11 +29,20 @@ def plot_2particleHO():
         print( i )
         path.append( "omega"+str(i)+"dim200Eigval.txt")
     number[0], stateomega0 = read2particle(path[0])
-    number[1], stateomega0 = read2particle(path[1])
-    number[2], stateomega0 = read2particle(path[2])
-    number[3], stateomega0 = read2particle(path[3])
+    number[1], stateomega1 = read2particle(path[1])
+    number[2], stateomega2 = read2particle(path[2])
+    number[3], stateomega3 = read2particle(path[3])
     step = 10./number
-    
+    prob1 = stateomega0**2; prob2 = stateomega1**2; prob3 = stateomega2**2; prob4 = stateomega3**2;
+    rho = np.linspace(0, 10, number[0])
+    plt.figure()
+    plt.plot(rho, prob1)
+    plt.plot(rho, prob2)
+    plt.plot(rho, prob3)
+    plt.plot(rho, prob4)
+    plt.xlabel(r"$\rho$")
+    plt.ylabel(r"$|\psi |^2$")
+    plt.show()
 
     return 0
 
